e081fe77a898c5e1a48a50eb662045944186ecbd
elin9763 <esotericlin@gmail.com>
2018-08-02 14:54:02 -0700

    fixed corrupt file after vb crash

--- db/migrate/20180801185404_create_lists.rb
+++ db/migrate/20180801185404_create_lists.rb
@@ -2,8 +2,8 @@ class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
       t.string :name
-
-      t.timestamps
+      
+      t.timestamps null: false
     end
   end
 end
