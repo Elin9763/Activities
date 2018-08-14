a154e7d99b20301a27a1525a4a2c9c34eccc48ba
elin9763 <esotericlin@gmail.com>
2018-08-13 21:59:35 -0700

    password digest and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_202240) do
+ActiveRecord::Schema.define(version: 2018_08_14_044454) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_202240) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
