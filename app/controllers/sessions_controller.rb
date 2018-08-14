129ba67912681b0f1ab8fd37de72050f6e2a1d58
elin9763 <esotericlin@gmail.com>
2018-08-14 13:00:04 -0700

    omniauth github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,35 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(:email => params[:email])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
+        if auth_hash = request.env["omniauth.auth"]
+            #Login with github
+            oauth_email = request.env["omniauth.auth"]["info"]["email"] #this is definitely this user
+            if user = User.find_by(:email => oauth_email)
+                #confirmed this is who they are
+                session[:user_id] = user.id
 
-            redirect_to root_path
+                redirect_to root_path
+            else
+                #first time login with oauth
+                user = User.new(:email => oauth_email, :password => SecureRandom.hex)
+                if user.save
+                    session[:user_id] = user.id
+
+                    redirect_to root_path
+                else
+                    raise user.errors.full_messages.inspect
+                end
+            end
         else
-            render 'sessions/new'
+            #Normal login username/pass
+            user = User.find_by(:email => params[:email])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+
+                redirect_to root_path
+            else
+                render 'sessions/new'
+            end
         end
     end
 
