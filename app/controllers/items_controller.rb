5c2237a899efa9dfb5fa91063a56381d195edf3b
elin9763 <esotericlin@gmail.com>
2018-08-14 22:36:22 -0700

    debug create in item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   def create
     @list = List.find(params[:list_id]) #finding the parent
     @item = @list.items.build(item_params)
+    @item.user_id = current_user.id
     if @item.save
       redirect_to list_path(@list)
     else
