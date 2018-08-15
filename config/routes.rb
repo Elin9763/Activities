2837bff5747668228bd6068488b6442fea1a0ec5
elin9763 <esotericlin@gmail.com>
2018-08-15 12:07:42 -0700

    added in welcome page, debug list delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
   
-  get '/login' => "sessions#new"
+  get '/signup' => "users#new", :as => "signup"
+  get '/login' => "sessions#new", :as => "login"
   post '/sessions' => "sessions#create"
   get '/logout' => "sessions#destroy"
   get '/auth/:provider/callback' => 'sessions#create'
   
+
   resources :users, :only => [:new, :create] #<-- created 7 restful routes but only allows 2(new and create)
+    #resources :lists
+  #end
 
   resources :lists do 
     resources :items
   end
 
   
-  root 'lists#index'
+  root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
